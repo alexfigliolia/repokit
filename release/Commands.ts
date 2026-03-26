83b5d984683b8642d27228e9d9019f9f0e99a70c
Alex Figliolia <alexfigliolia@gmail.com>
2026-03-26 01:07:57 -0700

    Setup repokit commands for release

---
+++ release/Commands.ts
@@ -0,0 +1,21 @@
+import { RepoKitCommand } from "@repokit/core";
+
+export const Commands = new RepoKitCommand({
+  name: "release",
+  owner: "Alex Figliolia",
+  description: "Release workflows for repokit",
+  commands: {
+    patch: {
+      command: "yarn tsx $(pwd)/run.ts -t patch",
+      description: `Bumps the patch version and lints the code base`,
+    },
+    minor: {
+      command: "yarn tsx $(pwd)/run.ts -t minor",
+      description: `Bumps the minor version and lints the code base`,
+    },
+    major: {
+      command: "yarn tsx $(pwd)/run.ts -t major",
+      description: `Bumps the major version and lints the code base`,
+    },
+  },
+});
---
+++ release/run.ts
@@ -0,0 +1,5 @@
+import { Release } from "./Release";
+
+void (async () => {
+  await Release.run();
+})();
