d7c39636a5d58cf35d3db4aa7f0d959dcc5d2e1a
Alex Figliolia <alexfigliolia@gmail.com>
2026-03-30 22:07:50 -0700

    Support multiline dot files

--- installation/install.sh
+++ installation/install.sh
@@ -36,8 +36,14 @@ if [ -f .repokit ]; then
     fi
 fi
 
-touch ".repokit"
-printf "$CURRENT_VERSION" > ".repokit"
+DOT_FILE=".repokit"
+TEMP_FILE=".repokit_tmp";
+
+touch "$DOT_FILE"
+
+printf "$CURRENT_VERSION\n" > "$TEMP_FILE"
+tail +2 "$DOT_FILE" >> "$TEMP_FILE"
+mv "$TEMP_FILE" "$DOT_FILE"
 
 
 cd $CWD
