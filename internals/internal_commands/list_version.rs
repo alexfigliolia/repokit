bdb94e6584702a20af7c9160217a75c99c9da83b
Alex Figliolia <alexfigliolia@gmail.com>
2026-08-19 20:42:08 -0700

    Patch version bump

--- internals/internal_commands/list_version.rs
+++ internals/internal_commands/list_version.rs
@@ -10,7 +10,7 @@ use crate::{
     logger::logger::Logger,
 };
 
-pub static REPOKIT_VERSION: &str = "5.1.5";
+pub static REPOKIT_VERSION: &str = "5.1.6";
 
 pub struct ListVersion {
     pub definition: InternalExecutableDefinition,
